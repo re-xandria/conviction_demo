3dc9cfe6a6105bc8a154a100be9e7e59b9ad6adc
Xandria <xcrosl1@wgu.edu>
2026-08-12 20:00:31 -0400

    platforms spawn in groups

--- Assets/Scripts/Controllers/Controller_PlatformSpawner.cs
+++ Assets/Scripts/Controllers/Controller_PlatformSpawner.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class Controller_PlatformSpawner : MonoBehaviour
 {
 
-    public GameObject platformPrefab;
-    public int numPlatforms = 10;
+    public GameObject platformUnitPrefab;
+    public int numPlatforms = 5;
    
-    public float platformXSpread = 10f;
-    public float platformYSpread = 0f;
+    public float platformXSpread = 0f;
+    public float platformYSpread = 5f;
     public float platformZSpread = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +15,7 @@ public class Controller_PlatformSpawner : MonoBehaviour
     {
         for (int i=0; i < numPlatforms; i++)
         {
-            SpreadPlatform();
+            SpreadPlatforms();
         }
     }
 
@@ -25,10 +25,36 @@ public class Controller_PlatformSpawner : MonoBehaviour
         
     }
 
-    void SpreadPlatform()
+    void SpreadPlatforms()
     {
-        Vector3 randPosition = new Vector3(Random.Range(-platformXSpread, platformXSpread), Random.Range(-platformYSpread, platformYSpread), Random.Range(-platformZSpread, platformZSpread)) + transform.position;
-        GameObject clone = Instantiate(platformPrefab, randPosition, Quaternion.identity);
+
+        // Create first platform, then randomly pick a number from 1-4 and add that many more platforms offset by the width of the platform prefab
+
+        int numUnits = Random.Range(1, 4);
+        Vector3 startPosition = new Vector3(Random.Range(-platformXSpread, platformXSpread), Random.Range(-platformYSpread, platformYSpread), Random.Range(0, platformZSpread)) + transform.position;
+        Vector3 updatedPosition = startPosition;
+
+        Instantiate(platformUnitPrefab, startPosition, Quaternion.identity);
+        
+        for (int i=0; i < numUnits; i++)
+        {
+            updatedPosition = new Vector3(updatedPosition.x, updatedPosition.y, updatedPosition.z + platformUnitPrefab.transform.localScale.z);
+            Instantiate(platformUnitPrefab, updatedPosition, Quaternion.identity);
+        }
     }
 
 }
+
+
+/*
+
+    Created simple platform spawner
+
+    TO DO
+    - Constrict platforms to same x position as player
+    - Define rules for platform generation
+    - Implement rules for platform generation
+    - Configure seed # to dictate platform generation
+
+*/
+
